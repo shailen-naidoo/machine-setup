c2ed41f430fe4c8027f5b6125e1f948a6e5efd86
Shailen Naidoo <naidooshailen648@gmail.com>
2023-11-08 21:19:02 +0200

    setup.sh
    * Added installation script for Homebrew, this is the package manager that I choose to use for MacOS
    * Added the installation script for Git
    * Added some comments to make it more readable

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
+# Install Homebrew Package Manager for MacOS
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install required applications
+brew install git
+
+# Setup for Git
 git config --global user.name "Shailen Naidoo"
 git config --global user.email "naidooshailen648@gmail.com"
