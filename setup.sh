6ce6f2738cbace209db0093bd3cf557b02c7f6ca
Shailen Naidoo <shailen.naidoo@sovtech.com>
2024-06-04 08:59:09 +0200

    setup.sh
    * Adding the path to brew
    * Installing nvm to manage the node version
    * Adding the path to nvm
    * Installing the LTS version of Node
    * Updated my email address to reflect my SovTech email. I guess I could make this an environment variable

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
 # Install Homebrew Package Manager for MacOS
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/shailennaidoo/.zprofile
+   eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Install required applications
 brew install git
+brew install nvm
+
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm' >> ~/.zshrc
+echo '[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion' >> ~/.zshrc
+
+source ~/.zshrc
+
+nvm install --lts
+nvm use --lts
 
 # Setup for Git
 git config --global user.name "Shailen Naidoo"
-git config --global user.email "naidooshailen648@gmail.com"
+git config --global user.email "shailen.naidoo@sovtech.com"
