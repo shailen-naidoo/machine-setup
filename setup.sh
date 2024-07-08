ea541a8223d2fa7ca2789de6e4f6dcd04a503bf6
Shailen <naidooshailen648@gmail.com>
2024-07-08 19:05:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,5 +18,5 @@ nvm install --lts
 nvm use --lts
 
 # Setup for Git
-git config --global user.name "Shailen Naidoo"
-git config --global user.email "naidooshailen648@gmail.com"
+git config --global user.name "${GIT_USERNAME}"
+git config --global user.email "${GIT_EMAIL}"
